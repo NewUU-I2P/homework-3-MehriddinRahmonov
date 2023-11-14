4191b33e92d7df8e58bdc6e9995ccd0ac40df178
MehriddinRahmonov <147691333+MehriddinRahmonov@users.noreply.github.com>
2023-11-14 21:43:10 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,18 +1,18 @@
 float problemSolution1(float consumed_water) {
-    float cost,fixed;
-    fixed = 13;
+    float cost,fixed_amount;
+    fixed_amount = 13;
     // write your code here
     if (consumed_water <= 30) {
-        cost = fixed + (consumed_water * 0.4);
+        cost = fixed_amount + (consumed_water * 0.4);
     }
-    else if (consumed_water > 30 and consumed_water <= 50) {
-        cost = fixed + ((consumed_water - 30) * 0.12) + (30 * 0.4);
+    else if (consumed_water > 30 && consumed_water <= 50) {
+        cost = fixed_amount + ((consumed_water - 30) * 0.12) + (30 * 0.4);
     }
-    else if (consumed_water > 50 and consumed_water <= 60) { // Fix: Changed "30" to "50" in the condition
-        cost = fixed + ((consumed_water - 50) * 1.4) + (30 * 0.4) + (20 * 0.12); // Fix: Changed "04" to "0.4"
+    else if (consumed_water > 50 && consumed_water <= 60) { 
+        cost = fixed_amount + ((consumed_water - 50) * 1.4) + (30 * 0.4) + (20 * 0.12);
     }
     else if (consumed_water > 60) {
-        cost = fixed + ((consumed_water - 60) * 1.5) + (30 * 0.4) + (20 * 0.12) + (10 * 1.4); // Fix: Corrected the formula
+        cost = fixed_amount + ((consumed_water - 60) * 1.5) + (30 * 0.4) + (20 * 0.12) + (10 * 1.4); 
     }
     return cost;
 }
